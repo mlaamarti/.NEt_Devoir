69d80bdd44d00d10afc206e888090f22fbd3e4d0
mlaamarti <laamarti.lm@yahoo.com>
2020-04-11 14:46:40 +0100

    .Net Devoir

---
+++ Savesalaire/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Savesalaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.solde2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.solde1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(186, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 38);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Effectuer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(35, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(145, 18);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Montant à verser :";
+            // 
+            // solde2
+            // 
+            this.solde2.AutoSize = true;
+            this.solde2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde2.Location = new System.Drawing.Point(219, 69);
+            this.solde2.Name = "solde2";
+            this.solde2.Size = new System.Drawing.Size(85, 24);
+            this.solde2.TabIndex = 15;
+            this.solde2.Text = "ResSave";
+            this.solde2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(107, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 24);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Saving : ";
+            // 
+            // solde1
+            // 
+            this.solde1.AutoSize = true;
+            this.solde1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.solde1.Location = new System.Drawing.Point(219, 16);
+            this.solde1.Name = "solde1";
+            this.solde1.Size = new System.Drawing.Size(100, 24);
+            this.solde1.TabIndex = 13;
+            this.solde1.Text = "ResSalaire";
+            this.solde1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Salary  : ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(186, 120);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(155, 28);
+            this.textBox1.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 357);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.solde2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.solde1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label solde2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label solde1;
+    }
+}
+
---
+++ Savesalaire/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+namespace Savesalaire
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+            BANKEntities ctx = new BANKEntities();
+            List<SalaryAccount> s = ctx.SalaryAccount.ToList<SalaryAccount>();
+            List<SavingAccount> s1 = ctx.SavingAccount.ToList<SavingAccount>();
+
+            solde1.Text = s[0].Balance.ToString();
+            solde2.Text = s1[0].Balance.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            int a = int.Parse(textBox1.Text);
+
+            using (var ctx = new BANKEntities())
+            {
+                List<SalaryAccount> s = ctx.SalaryAccount.ToList<SalaryAccount>();
+                List<SavingAccount> s1 = ctx.SavingAccount.ToList<SavingAccount>();
+                using (DbContextTransaction tr = ctx.Database.BeginTransaction())
+                {
+                    int? x=s[0].Balance - a;
+                    try
+                    {
+                        if (x >= 0)
+                        {
+
+                            s[0].Balance -= a;
+                            s1[0].Balance += a;
+
+                            ctx.SaveChanges();
+
+                            MessageBox.Show("Transaction success");
+
+                            tr.Commit();
+                            textBox1.Clear();
+                        }
+                        else
+                        {
+                            MessageBox.Show("tu peux pas faire cette operation a cause vous avez pas d solde qui contient le montant entrer");
+                            
+                        }
+
+                        
+                    }
+                    catch(Exception ex)
+
+                    {
+                        MessageBox.Show("Transaction Fail "+ex.Message);
+                        tr.Rollback();
+
+                    }
+
+                    refresh();
+                   
+                
+                  
+
+                }
+            }
+
+
+        }
+    void refresh()
+        {
+            solde1.Text = "";
+            solde2.Text = "";
+
+            solde1.Text = s[0].Balance.ToString();
+            solde2.Text = s1[0].Balance.ToString();
+        }
+    }
+}
